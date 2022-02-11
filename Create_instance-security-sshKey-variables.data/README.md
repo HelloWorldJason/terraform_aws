c4eb5ec4b41a02676cb1241184b5389b00aa7c99
Your Name <you@example.com>
2022-02-11 18:48:02 +0800

    terrform增加data查询功能

---
+++ Create_instance-security-sshKey-variables.data/README.md
@@ -0,0 +1,8 @@
+# Terraform for AWS
+- 初次学习terraform后期会不断进行补充,希望有更多的中文资料已经实战内容提供出来,产考使用.
+
+## 创建实例并初始化秘钥登录以及绑定安全组
+文件中具体做了描述
+
+## FAQ
+https://terraform.io/docs/providers/aws/index.html
---
+++ Create_instance-security-sshKey-variables.data/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+    region = var.region  #通过variables文件获取信息
+}
+
+resource "aws_instance" "this" {
+    ami = lookup(var.amis, var.region)    #这里可能难理解，为什么括号中两个值，那是以为lookup的内容是一个map类型,map中需要查看region的key中的values值  
+    instance_type = var.instance_type            
+    key_name = aws_key_pair.ssh.key_name  #key_name是aws_instance中的一个参数，是指实例配置配置秘钥登录配置初始化主机。 
+    user_data = file("set_nginx.sh")      #读取同级别目录下的文件
+
+    tags = {   # tags主要在EC2中的tag进行加一个tags标签，方便查看读取维护.
+        Name = "Nginx-web-server" 
+    }
+}
+
+data "aws_security_groups" "default" {     #data内置函数主要作用是查询功能，不做执行操作
+    filter {
+      name = "group-name"
+      values = ["default"]
+    }
+    # filter主要做过滤，通过aws_security_groups接口获取到安全组的列表，通过values来获取我们要的default的值。
+}
+
+
+#添加ssh秘钥
+resource "aws_key_pair" "ssh" {
+    key_name = "jason"        #安全组中key的名字
+    public_key = file(var.public_key)
+}
+
+#开放22端口
+resource "aws_security_group_rule" "ssh" {
+    type = "ingress"    #ingress是入站，出站是egress
+    from_port = 22      #启始端口
+    to_port = 22        #结束端口
+    protocol = "tcp"    #连接协议tcp/udp/http/all
+    cidr_blocks = ["0.0.0.0/0"]   #允许访问的网段
+    security_group_id =  data.aws_security_groups.default.ids[0]    #ids[0]意思是拿第一个值
+}
+
+#开放80端口
+resource "aws_security_group_rule" "web" {
+    type = "ingress"    #ingress是入站，出站是egress
+    from_port = 80      #启始端口
+    to_port = 80        #结束端口
+    protocol = "tcp"    #连接协议tcp/udp/http/all
+    cidr_blocks = ["0.0.0.0/0"]   #允许访问的网段
+    security_group_id =  data.aws_security_groups.default.ids[0]   #ids[0]意思是拿第一个值
+}
